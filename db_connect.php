4fd9d565a03f6588509ead261463339e09bc2e70
Skrivute <aquinoclintmark@gmail.com>
2025-12-09 09:43:51 +0800

    null

--- db_connect.php
+++ db_connect.php
@@ -3,7 +3,7 @@ $db_host = 'localhost';
 $db_user = 'root';
 $db_pass = '';
 $db_name = 'swap_share_db';
-$db_port = 3306;
+$db_port = 3307;
 
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
 
@@ -15,4 +15,3 @@ if ($conn->connect_error) {
     ]);
     exit;
 }
-?>
